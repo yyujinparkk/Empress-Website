35ad2d06ebef8a9a36b56c18223dccc4bede41a7
yyujinparkk <yujin23841@gmail.com>
2024-07-14 18:30:40 -0400

    Updated navbar and homepage

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,9 @@ import React, {useState} from 'react'
 import {Link, NavLink} from "react-router-dom"
 import "./Navbar.css"
 import EmpressLogo from "../assessts/EmpressLogo.png"
+import IgLogo from "../assessts/instagram-white-icon.png"
+import LinkedInLogo from "../assessts/linkedin_logo_white2.png"
+import FacebookLogo from "../assessts/facebook_logo_white.png"
 
 export const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false)
@@ -22,10 +25,10 @@ export const Navbar = () => {
             </NavLink>
         </li>
         <li>
-          <NavLink to="/products">Products</NavLink>
+          <NavLink to="/about">About</NavLink>
         </li>
         <li>
-          <NavLink to="/events">Events</NavLink>
+          <NavLink to="/events">Recruitment</NavLink>
         </li>
       </ul>
       <Link to="/" className='EmpressLogo'>
@@ -33,10 +36,10 @@ export const Navbar = () => {
       </Link>
       <ul className={menuOpen ? "open right-menu" : "right-menu"}>
         <li>
-          <NavLink to="/about">About</NavLink>
+          <NavLink to="/about">Members</NavLink>
         </li>
         <li>
-          <NavLink to="/services">Services</NavLink>
+          <NavLink to="/services">Gallery</NavLink>
         </li>
         <li>
          <NavLink to="/contact">Contact</NavLink>
@@ -62,6 +65,21 @@ export const Navbar = () => {
           <NavLink to="/contact">Contact</NavLink>
         </li>
       </ul>
+      <div>
+      <Link to="https://www.instagram.com/empressinterestgroup/?hl=en" className='iglogo'>
+        <img src={IgLogo} alt="Instagram Logo" />
+      </Link>
+      </div>
+      <div>
+      <Link to="https://www.linkedin.com/company/empress-interest-group/" className='linkedin_logo'>
+        <img src={LinkedInLogo} alt="LinkedIn Logo" />
+      </Link>
+      </div>
+      <div>
+      <Link to="https://www.facebook.com/ufempress/" className='facebook_logo'>
+        <img src={FacebookLogo} alt="Facebook Logo" />
+      </Link>
+      </div>
     </nav>
   );
 };
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const About = () => {
   return (
     <div>
-      About
+      WHO WE ARE
     </div>
   )
 }
